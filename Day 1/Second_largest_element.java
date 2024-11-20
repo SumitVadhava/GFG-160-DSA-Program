eae6938dbd22754f1a8c5ff6f2a599f86ae451bf
Sumit  Vadhava <140806339+SumitVadhava@users.noreply.github.com>
2024-11-20 17:21:31 +0530

    Add files via upload

---
+++ Day 1/Second_largest_element.java
@@ -0,0 +1,28 @@
+public class Second_largest_element{
+    static int getSecondLargest(int[] arr) {
+
+        int largestElement = -1,  SecondLargestElement = -1;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if(arr[i] > largestElement) {
+                SecondLargestElement = largestElement;
+                largestElement = arr[i];
+            }
+          
+            else if(arr[i] < largestElement && arr[i] > SecondLargestElement) {
+                SecondLargestElement = arr[i];
+            }
+        }
+        return SecondLargestElement;
+    }
+    public static void main(String[] args) {
+      
+        int [] arr = new int[]{1,5,9,35,34,7};
+
+        int ans = getSecondLargest(arr);
+
+        System.out.print(ans);
+
+    }
+}
---
+++ Day 2/move_all_zero_to_end.java
@@ -0,0 +1,25 @@
+public class move_all_zero_to_end{
+    static void pushZerosToEnd(int[] arr) {
+        
+       int index = 0;
+
+        for(int i = 0 ; i < arr.length ; i++){
+            if(arr[i]!=0){
+                arr[index] = arr[i];
+                index++;
+            }
+        }
+        for(int j = index ; j < arr.length ; j++){
+            arr[j] = 0;
+        }
+    }
+    public static void main(String[] args) {
+
+        int []arr = new int[]{1,2,0,5,3,8,7,0,0,6,9,0};
+        pushZerosToEnd(arr);
+
+        for(int i = 0; i < arr.length ; i++){
+            System.out.print(arr[i] +" ");
+        }
+    }
+}
